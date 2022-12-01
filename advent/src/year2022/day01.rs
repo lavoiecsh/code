5039442fb083e55e8b7711df8d2270b1cc75645f
Sebastien Lavoie-Courchesne <lavoiecsh@gmail.com>
2022-12-01 10:01:34 -0500

    year 2022, day 01 cleaner solution

--- advent/src/year2022/day01.rs
+++ advent/src/year2022/day01.rs
@@ -7,21 +7,12 @@ pub struct Advent2022Day01Solver {
 
 impl Advent2022Day01Solver {
     pub fn new() -> Self {
-        let mut calories = Vec::new();
-        let text = read_to_string("src/year2022/day01.txt").unwrap();
-        let lines: Vec<&str> = text.trim().lines().collect();
-        let mut elf: Vec<usize> = Vec::new();
-        for line in lines {
-            if line.is_empty() {
-                calories.push(elf);
-                elf = Vec::new();
-            } else {
-                elf.push(line.parse().unwrap());
-            }
-        }
-        calories.push(elf);
         Self {
-            calories
+            calories: read_to_string("src/year2022/day01.txt")
+                .unwrap()
+                .split("\n\n")
+                .map(|e| e.lines().map(|c| c.parse().unwrap()).collect())
+                .collect()
         }
     }
 }
@@ -41,6 +32,6 @@ impl AdventSolver for Advent2022Day01Solver {
     fn solve_part2(&self) -> usize {
         let mut elves: Vec<usize> = self.calories.iter().map(|e| e.iter().sum::<usize>()).collect();
         elves.sort();
-        elves.iter().skip(elves.len() - 3).sum()
+        elves.iter().rev().take(3).sum()
     }
 }
