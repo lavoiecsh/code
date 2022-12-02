b688ae320777f7696070f92469b40c9637600a78
Sebastien Lavoie-Courchesne <lavoiecsh@gmail.com>
2022-12-02 00:15:12 -0500

    year 2022, day 02

---
+++ advent/src/year2022/day02.rs
@@ -0,0 +1,75 @@
+use std::fs::read_to_string;
+use crate::solver::AdventSolver;
+
+pub struct Advent2022Day02Solver {
+    rounds: Vec<(char, char)>
+}
+
+impl Advent2022Day02Solver {
+    pub fn new() -> Self {
+        Self {
+            rounds: read_to_string("src/year2022/day02.txt")
+                .unwrap()
+                .trim()
+                .lines()
+                .map(|l| l.chars().collect::<Vec<char>>())
+                .map(|c| (c[0], c[2]))
+                .collect()
+        }
+    }
+}
+
+impl AdventSolver for Advent2022Day02Solver {
+    fn day(&self) -> usize { 02 }
+    fn year(&self) -> usize { 2022 }
+
+    fn solve_part1(&self) -> usize {
+        self.rounds
+            .iter()
+            .map(convert_part1)
+            .map(score)
+            .sum()
+    }
+
+    fn solve_part2(&self) -> usize {
+        self.rounds
+            .iter()
+            .map(convert_part2)
+            .map(score)
+            .sum()
+    }
+}
+
+fn convert_part1(round: &(char, char)) -> (char, char) {
+    (round.0, match round.1 { 'X' => 'A', 'Y' => 'B', 'Z' => 'C', _ => panic!("unknown") })
+}
+
+fn convert_part2(round: &(char, char)) -> (char, char) {
+    (round.0, match round {
+        ('A','X') => 'C',
+        ('A','Y') => 'A',
+        ('A','Z') => 'B',
+        ('B','X') => 'A',
+        ('B','Y') => 'B',
+        ('B','Z') => 'C',
+        ('C','X') => 'B',
+        ('C','Y') => 'C',
+        ('C','Z') => 'A',
+        _ => panic!("unknown")
+    })
+}
+
+fn score(round: (char, char)) -> usize {
+    match round {
+        ('A','A') => 1 + 3,
+        ('A','B') => 2 + 6,
+        ('A','C') => 3 + 0,
+        ('B','A') => 1 + 0,
+        ('B','B') => 2 + 3,
+        ('B','C') => 3 + 6,
+        ('C','A') => 1 + 6,
+        ('C','B') => 2 + 0,
+        ('C','C') => 3 + 3,
+        _ => 0
+    }
+}
--- advent/src/year2022/mod.rs
+++ advent/src/year2022/mod.rs
@@ -1,10 +1,13 @@
 use crate::solver::AdventSolverBuilder;
 use crate::year2022::day01::{Advent2022Day01Solver};
+use crate::year2022::day02::Advent2022Day02Solver;
 
 mod day01;
+mod day02;
 
 pub fn advent2022_solver_builders() -> Vec<AdventSolverBuilder> {
     vec!(
         || Box::new(Advent2022Day01Solver::new()),
+        || Box::new(Advent2022Day02Solver::new()),
     )
 }
