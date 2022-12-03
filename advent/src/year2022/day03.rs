c23b696e5491df45bedc7073a6c4749e70eb86bb
Sebastien Lavoie-Courchesne <lavoiecsh@gmail.com>
2022-12-03 00:19:44 -0500

    year 2022, day 03

---
+++ advent/src/year2022/day03.rs
@@ -0,0 +1,74 @@
+use std::fs::read_to_string;
+use itertools::Itertools;
+use crate::solver::AdventSolver;
+
+pub struct Advent2022Day03Solver {
+    rucksacks: Vec<String>,
+}
+
+impl Advent2022Day03Solver {
+    pub fn new() -> Self {
+        Self {
+            rucksacks: read_to_string("src/year2022/day03.txt")
+                .unwrap()
+                .trim()
+                .lines()
+                .map(|l| l.to_string())
+                .collect()
+        }
+    }
+}
+
+impl AdventSolver for Advent2022Day03Solver {
+    fn day(&self) -> usize { 03 }
+    fn year(&self) -> usize { 2022 }
+
+    fn solve_part1(&self) -> usize {
+        self.rucksacks
+            .iter()
+            .map(compartmentalize)
+            .map(identify_duplicate)
+            .map(prioritize)
+            .sum()
+    }
+
+    fn solve_part2(&self) -> usize {
+        self.rucksacks
+            .iter()
+            .chunks(3)
+            .into_iter()
+            .map(|c| c.map(|s| s.clone()).collect::<Vec<String>>())
+            .map(identify_triplicate)
+            .map(prioritize)
+            .sum()
+    }
+}
+
+fn compartmentalize(rucksack: &String) -> (Vec<char>, Vec<char>) {
+    let half_length = rucksack.len() / 2;
+    (rucksack.chars().take(half_length).collect(), rucksack.chars().skip(half_length).collect())
+}
+
+fn identify_duplicate(compartments: (Vec<char>, Vec<char>)) -> char {
+    *compartments.0
+        .iter()
+        .find(|c| compartments.1.contains(c))
+        .unwrap()
+}
+
+fn identify_triplicate(rucksacks: Vec<String>) -> char {
+    rucksacks[0]
+        .chars()
+        .filter(|c| rucksacks[1].contains(*c))
+        .filter(|c| rucksacks[2].contains(*c))
+        .next()
+        .unwrap()
+}
+
+fn prioritize(item: char) -> usize {
+    if item.is_uppercase() {
+        item as usize - 65 + 27
+    } else {
+        item as usize - 97 + 1
+    }
+}
--- advent/src/year2022/mod.rs
+++ advent/src/year2022/mod.rs
@@ -1,13 +1,16 @@
 use crate::solver::AdventSolverBuilder;
 use crate::year2022::day01::{Advent2022Day01Solver};
 use crate::year2022::day02::Advent2022Day02Solver;
+use crate::year2022::day03::Advent2022Day03Solver;
 
 mod day01;
 mod day02;
+mod day03;
 
 pub fn advent2022_solver_builders() -> Vec<AdventSolverBuilder> {
     vec!(
         || Box::new(Advent2022Day01Solver::new()),
         || Box::new(Advent2022Day02Solver::new()),
+        || Box::new(Advent2022Day03Solver::new()),
     )
 }
