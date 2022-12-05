95b40e540b828331704f1a6b47bc0ece7bb5cb66
Sebastien Lavoie-Courchesne <lavoiecsh@gmail.com>
2022-12-05 00:31:01 -0500

    year 2022, day 05

---
+++ advent/src/year2022/day05.rs
@@ -0,0 +1,104 @@
+use std::fs::read_to_string;
+use regex::Regex;
+use crate::solver::AdventSolver;
+
+#[derive(Debug)]
+struct CraneOperation {
+    count: usize,
+    from: usize,
+    to: usize,
+}
+
+struct Yard {
+    stacks: Vec<Vec<char>>
+}
+
+impl Yard {
+    fn new(stacks: Vec<Vec<char>>) -> Self {
+        Self { stacks }
+    }
+
+    fn execute_single(&mut self, operation: &CraneOperation) {
+        for _ in 0..operation.count {
+            let c = self.stacks[operation.from].pop().unwrap();
+            self.stacks[operation.to].push(c);
+        }
+    }
+
+    fn execute_multi(&mut self, operation: &CraneOperation) {
+        let mut tmp = Vec::new();
+        for _ in 0..operation.count {
+            tmp.push(self.stacks[operation.from].pop().unwrap());
+        }
+        while !tmp.is_empty() {
+            self.stacks[operation.to].push(tmp.pop().unwrap());
+        }
+    }
+}
+
+pub struct Advent2022Day05Solver {
+    starting_stacks: Vec<Vec<char>>,
+    operations: Vec<CraneOperation>,
+}
+
+impl Advent2022Day05Solver {
+    pub fn new() -> Self {
+        let text = read_to_string("src/year2022/day05.txt").unwrap().lines().map(|l|l.to_string()).collect::<Vec<String>>();
+        let mut stacks = Vec::new();
+        for _ in 1..=9 {
+            stacks.push(Vec::new());
+        }
+        for i in (0..8).rev() {
+            let chars = text[i].clone().chars().collect::<Vec<char>>();
+            for s in 0..9 {
+                let c = s*4 + 1;
+                if chars.len() > c && chars[c] != ' ' {
+                    stacks[s].push(chars[c]);
+                }
+            }
+        }
+        let operation_regex = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
+        Self {
+            starting_stacks: stacks,
+            operations: text
+                .iter()
+                .skip(10)
+                .map(|l| {
+                    let m = operation_regex.captures(l).unwrap();
+                    CraneOperation {
+                        count: m.get(1).unwrap().as_str().parse().unwrap(),
+                        from: m.get(2).unwrap().as_str().parse::<usize>().unwrap() - 1,
+                        to: m.get(3).unwrap().as_str().parse::<usize>().unwrap() - 1,
+                    }
+                })
+                .collect()
+        }
+    }
+}
+
+impl AdventSolver for Advent2022Day05Solver {
+    fn day(&self) -> usize { 05 }
+    fn year(&self) -> usize { 2022 }
+
+    fn solve_part1_string(&self) -> String {
+        let mut yard = Yard::new(self.starting_stacks.clone());
+        self.operations
+            .iter()
+            .for_each(|o| yard.execute_single(o));
+        yard.stacks
+            .iter()
+            .map(|s| s.last().unwrap())
+            .collect()
+    }
+
+    fn solve_part2_string(&self) -> String {
+        let mut yard = Yard::new(self.starting_stacks.clone());
+        self.operations
+            .iter()
+            .for_each(|o| yard.execute_multi(o));
+        yard.stacks
+            .iter()
+            .map(|s| s.last().unwrap())
+            .collect()
+    }
+}
--- advent/src/year2022/mod.rs
+++ advent/src/year2022/mod.rs
@@ -1,13 +1,15 @@
 use crate::solver::AdventSolverBuilder;
-use crate::year2022::day01::{Advent2022Day01Solver};
+use crate::year2022::day01::Advent2022Day01Solver;
 use crate::year2022::day02::Advent2022Day02Solver;
 use crate::year2022::day03::Advent2022Day03Solver;
 use crate::year2022::day04::Advent2022Day04Solver;
+use crate::year2022::day05::Advent2022Day05Solver;
 
 mod day01;
 mod day02;
 mod day03;
 mod day04;
+mod day05;
 
 pub fn advent2022_solver_builders() -> Vec<AdventSolverBuilder> {
     vec!(
@@ -15,5 +17,6 @@ pub fn advent2022_solver_builders() -> Vec<AdventSolverBuilder> {
         || Box::new(Advent2022Day02Solver::new()),
         || Box::new(Advent2022Day03Solver::new()),
         || Box::new(Advent2022Day04Solver::new()),
+        || Box::new(Advent2022Day05Solver::new()),
     )
 }
