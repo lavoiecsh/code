a2eae2ca20e4b1171646f9e247118411a74cac30
Sebastien Lavoie-Courchesne <lavoiecsh@gmail.com>
2022-12-07 01:05:07 -0500

    year 2022, day 07

---
+++ advent/src/year2022/day07.rs
@@ -0,0 +1,124 @@
+use std::collections::HashMap;
+use std::fs::read_to_string;
+use regex::Regex;
+use crate::solver::AdventSolver;
+
+struct D {
+    name: String,
+    parent: Option<usize>,
+    children: Vec<usize>,
+    files: Vec<F>,
+}
+
+impl D {
+    fn new(name: String, parent: Option<usize>) -> Self {
+        Self { name, parent, children: Vec::new(), files: Vec::new() }
+    }
+
+    fn file_sizes(&self) -> usize {
+        self.files.iter().map(|f| f.size).sum()
+    }
+}
+
+struct F {
+    name: String,
+    size: usize,
+}
+
+pub struct Advent2022Day07Solver {
+    directories: Vec<D>,
+}
+
+impl Advent2022Day07Solver {
+    fn compute_directory_sizes(&self) -> HashMap<usize, usize> {
+        let mut sizes: HashMap<usize, usize> = HashMap::new();
+        while sizes.len() != self.directories.len() {
+            for di in 0..self.directories.len() {
+                if sizes.contains_key(&di) {
+                    continue;
+                }
+                let d = &self.directories[di];
+                if d.children.is_empty() {
+                    sizes.insert(di, d.file_sizes());
+                    continue;
+                }
+                let children_sizes = d.children
+                    .iter()
+                    .map(|ci| sizes.get(ci))
+                    .fold(Some(0 as usize), |acc, cur| if cur.is_none() { None } else { acc.map(|a| a + cur.unwrap()) });
+                if children_sizes.is_some() {
+                    sizes.insert(di, d.file_sizes() + children_sizes.unwrap());
+                }
+            }
+        }
+        sizes
+    }
+}
+
+impl Advent2022Day07Solver {
+    pub fn new() -> Self {
+        let command_regex = Regex::new(r"\$ (cd|ls) ?(.*)?").unwrap();
+        let mut directories = vec!(D::new(String::from("/"), None));
+        let mut directories_len = 1;
+        let mut wdi = 0;
+        for line in read_to_string("src/year2022/day07.txt").unwrap().lines() {
+            let command_captures = command_regex.captures(line);
+            if command_captures.is_some() {
+                let c = command_captures.unwrap();
+                if c.get(1).unwrap().as_str() == "ls" {
+                    continue;
+                }
+                let to = c.get(2).unwrap().as_str();
+                if to == "/" {
+                    wdi = 0;
+                } else if to == ".." {
+                    wdi = directories[wdi].parent.unwrap();
+                } else {
+                    wdi = *directories[wdi].children.iter().find(|cdi| directories[**cdi].name == to).unwrap();
+                }
+                continue;
+            }
+            // ls
+            let s = line.split(" ").map(String::from).collect::<Vec<String>>();
+            if s[0] == "dir" {
+                directories.push(D::new(s[1].clone(), Some(wdi)));
+                directories[wdi].children.push(directories_len);
+                directories_len += 1;
+            } else {
+                directories[wdi].files.push(F { name: s[1].clone(), size: s[0].parse().unwrap() });
+            }
+        }
+        Self {
+            directories,
+        }
+    }
+}
+
+impl AdventSolver for Advent2022Day07Solver {
+    fn day(&self) -> usize { 07 }
+    fn year(&self) -> usize { 2022 }
+
+    fn solve_part1(&self) -> usize {
+        let max: usize = 100000;
+        let directory_sizes = self.compute_directory_sizes();
+        directory_sizes
+            .iter()
+            .map(|(_, s)| *s)
+            .filter(|s| *s <= max)
+            .sum()
+    }
+
+    fn solve_part2(&self) -> usize {
+        let max: usize = 70000000;
+        let need: usize = 30000000;
+        let directory_sizes = self.compute_directory_sizes();
+        let current = directory_sizes.get(&0).unwrap();
+        let to_free = need - (max - current);
+        directory_sizes
+            .iter()
+            .map(|(_, s)| *s)
+            .filter(|s| *s >= to_free)
+            .min()
+            .unwrap()
+    }
+}
--- advent/src/year2022/mod.rs
+++ advent/src/year2022/mod.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use crate::solver::AdventSolverBuilder;
 use crate::year2022::day01::Advent2022Day01Solver;
 use crate::year2022::day02::Advent2022Day02Solver;
@@ -5,6 +7,7 @@ use crate::year2022::day03::Advent2022Day03Solver;
 use crate::year2022::day04::Advent2022Day04Solver;
 use crate::year2022::day05::Advent2022Day05Solver;
 use crate::year2022::day06::Advent2022Day06Solver;
+use crate::year2022::day07::Advent2022Day07Solver;
 
 mod day01;
 mod day02;
@@ -12,6 +15,7 @@ mod day03;
 mod day04;
 mod day05;
 mod day06;
+mod day07;
 
 pub fn advent2022_solver_builders() -> Vec<AdventSolverBuilder> {
     vec!(
@@ -21,5 +25,6 @@ pub fn advent2022_solver_builders() -> Vec<AdventSolverBuilder> {
         || Box::new(Advent2022Day04Solver::new()),
         || Box::new(Advent2022Day05Solver::new()),
         || Box::new(Advent2022Day06Solver::new()),
+        || Box::new(Advent2022Day07Solver::new()),
     )
 }
