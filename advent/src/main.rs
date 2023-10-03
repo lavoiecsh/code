6d297d25e81a35f6a3974a31feb34c94924a1cc5
Sebastien Lavoie-Courchesne <lavoiecsh@gmail.com>
2023-10-03 11:41:13 -0400

    Simplify option parsing

--- advent/src/main.rs
+++ advent/src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Debug, Formatter};
 use std::fs::read_to_string;
 use std::time::SystemTime;
 
@@ -14,7 +15,7 @@ mod year2021;
 mod year2022;
 
 #[derive(Parser)]
-struct Cli {
+struct Options {
     #[arg(short = 'y', long, help("Year to solve, defaults to latest available year"))]
     year: Option<u16>,
     #[arg(short = 'd', long, help("Day to solve, defaults to latest solved problem within the year"))]
@@ -33,72 +34,83 @@ struct Cli {
     stdin: bool,
 }
 
+impl Options {
+    fn part1(&self) -> bool {
+        self.both_parts() || self.part1
+    }
+
+    fn part2(&self) -> bool {
+        self.both_parts() || self.part2
+    }
+
+    fn both_parts(&self) -> bool {
+        self.part1 == self.part2
+    }
+
+    fn read_input(&self, year: &str, day: &str) -> Result<String, AdventError> {
+        match (&self.file, &self.input, &self.stdin) {
+            (None, None, true) => {
+                let mut buffer = String::new();
+                std::io::stdin().read_line(&mut buffer).map_err(|_| AdventError::InvalidInputOptions)?;
+                Ok(buffer.trim().to_string())
+            },
+            (None, None, false) => read_to_string(format!("input/year{}/day{}.txt", year, day)).map(|i| i.trim().to_string()).map_err(|_| AdventError::InvalidInputOptions),
+            (Some(f), None, false) => read_to_string(f).map(|i| i.trim().to_string()).map_err(|_| AdventError::InvalidInputOptions),
+            (None, Some(i), false) => Ok(i.to_string()),
+            _ => Err(AdventError::InvalidInputOptions),
+        }
+    }
+}
+
 pub type AdventSolverBuilder = fn(input: String) -> Box<dyn AdventSolver>;
 
-#[derive(Debug)]
 pub enum AdventError {
     UnknownYear(u16),
     UnknownDay(u16, u8),
-    InvalidPartOptions,
     InvalidInputOptions,
 }
 
+impl Debug for AdventError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            AdventError::UnknownYear(y) => f.write_fmt(format_args!("Unknown year {}", y)),
+            AdventError::UnknownDay(y, d) => f.write_fmt(format_args!("Unknown day {} within year {}", d, y)),
+            AdventError::InvalidInputOptions => f.write_fmt(format_args!("Input options are not valid, only one of input, file and stdin can be specified at a time")),
+        }
+    }
+}
+
+macro_rules! time {
+    ($s: stmt) => {
+        let now = SystemTime::now();
+        $s
+        match now.elapsed() {
+            Ok(d) => println!("Duration: {}s {:0>3}.{:0>3}ms", d.as_secs(), d.subsec_millis(), d.subsec_micros() % 1000),
+            Err(_) => println!("Duration errored"),
+        }
+    }
+}
+
 fn main() -> Result<(), AdventError> {
-    let cli = Cli::parse();
+    let options = Options::parse();
 
-    let (solver_builder, year, day) = solver_builder(&cli.year, &cli.day)?;
-    let input = read_input(&cli, &year, &day)?;
+    let (solver_builder, year, day) = solver_builder(&options.year, &options.day)?;
+    let input = options.read_input(&year, &day)?;
 
-    println!("Building solver");
-    let solver = build_solver(solver_builder, input)?;
+    println!("Building solver for year {} day {}", &year, &day);
+    time!(let solver = solver_builder(input));
 
-    let (part1, part2) = read_parts(&cli)?;
-
-    if part1 {
+    if options.part1() {
         println!("Solving part 1");
-        let (solution, duration) = time(|| solver.solve_part1_string());
+        time!(let solution = solver.solve_part1_string());
         println!("Solution: {}", solution);
-        println!("Duration: {}", duration);
     }
 
-    if part2 {
+    if options.part2() {
         println!("Solving part 2");
-        let (solution, duration) = time(|| solver.solve_part2_string());
+        time!(let solution = solver.solve_part2_string());
         println!("Solution: {}", solution);
-        println!("Duration: {}", duration);
     }
 
     Ok(())
-}
-
-fn read_input(cli: &Cli, year: &str, day: &str) -> Result<String, AdventError> {
-    match (&cli.file, &cli.input, &cli.stdin) {
-        (None, None, true) => todo!(), // read from stdin,
-        (None, None, false) => read_to_string(format!("input/year{}/day{}.txt", year, day)).map_err(|_| AdventError::InvalidInputOptions),
-        (Some(f), None, false) => todo!(), // read from given file
-        (None, Some(i), false) => Ok(i.to_string()),
-        _ => Err(AdventError::InvalidInputOptions),
-    }
-}
-
-fn read_parts(cli: &Cli) -> Result<(bool, bool), AdventError> {
-    match (cli.part1, cli.part2) {
-        (false, false) => Ok((true, true)),
-        (false, true) => Ok((false, true)),
-        (true, false) => Ok((true, false)),
-        (true, true) => Err(AdventError::InvalidPartOptions)
-    }
-}
-
-fn build_solver(builder: AdventSolverBuilder, input: String) -> Result<Box<dyn AdventSolver>, AdventError> {
-    Ok(builder(input))
-}
-
-fn time<T>(func: impl Fn() -> T) -> (T, String) {
-    let now = SystemTime::now();
-    let result = func();
-    match now.elapsed() {
-        Ok(d) => (result, format!("{}s {:0>3}.{:0>3}ms", d.as_secs(), d.subsec_millis(), d.subsec_micros() % 1000)),
-        Err(e) => (result, format!("{:?}", e))
-    }
 }
