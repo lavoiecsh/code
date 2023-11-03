37607ad51d8f94722f7d997d95d6f8a6ea6703c7
Sebastien Lavoie-Courchesne <lavoiecsh@gmail.com>
2023-11-03 16:59:35 -0400

    year 2017 day 02

---
+++ advent/src/year2017/day02.rs
@@ -0,0 +1,41 @@
+use crate::solver::AdventSolver;
+
+pub struct Advent2017Day02Solver {
+    numbers: Vec<Vec<usize>>,
+}
+
+impl Advent2017Day02Solver {
+    pub fn new(input: String) -> Self {
+        Self {
+            numbers: input.lines()
+                .map(|l| l.split("\t").map(|n| n.parse().unwrap()).collect())
+                .collect()
+        }
+    }
+}
+
+impl AdventSolver for Advent2017Day02Solver {
+    fn solve_part1(&self) -> usize {
+        self.numbers.iter()
+            .map(|l| l.iter().max().unwrap() - l.iter().min().unwrap())
+            .sum()
+    }
+
+    fn solve_part2(&self) -> usize {
+        self.numbers.iter()
+            .map(|l| {
+                for i in 0..l.len() {
+                    for j in i+1..l.len() {
+                        if l[i] % l[j] == 0 {
+                            return l[i] / l[j]
+                        }
+                        if l[j] % l[i] == 0 {
+                            return l[j] / l[i]
+                        }
+                    }
+                }
+                0
+            })
+            .sum()
+    }
+}
