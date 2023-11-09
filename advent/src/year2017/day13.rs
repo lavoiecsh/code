297419890630257f3c03ce5f0c758fee26facbd7
Sebastien Lavoie-Courchesne <lavoiecsh@gmail.com>
2023-11-09 15:34:42 -0500

    year 2017 day 13

---
+++ advent/src/year2017/day13.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+use itertools::Itertools;
+use crate::solver::AdventSolver;
+
+pub struct Advent2017Day13Solver {
+    layers: HashMap<usize, usize>,
+}
+
+impl Advent2017Day13Solver {
+    pub fn new(input: String) -> Self {
+        Self {
+            layers: input.lines()
+                .map(|l| {
+                    l.split(": ").map(|n| n.parse().unwrap()).collect_tuple().unwrap()
+                })
+                .collect()
+        }
+    }
+}
+
+impl AdventSolver for Advent2017Day13Solver {
+    fn solve_part1(&self) -> usize {
+        self.layers.iter()
+            .filter(|(d,r)| *d % ((*r - 1) * 2) == 0)
+            .map(|(d,r)| d * r)
+            .sum()
+    }
+
+    fn solve_part2(&self) -> usize {
+        (0..)
+            .find(|i| self.layers.iter().all(|(d,r)| (i + d) % ((r - 1) * 2) != 0))
+            .unwrap()
+    }
+}
