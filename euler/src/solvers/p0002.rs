757df17277780743e542781023e3dda209ea5092
Sebastien Lavoie-Courchesne <lavoiecsh@gmail.com>
2024-03-28 21:42:54 -0400

    euler p0002

---
+++ euler/src/solvers/p0002.rs
@@ -0,0 +1,21 @@
+pub fn p0002_solver() -> usize {
+    sum_of_even_fibonacci_numbers(4000000)
+}
+
+fn sum_of_even_fibonacci_numbers(under: usize) -> usize {
+    let mut a = 1;
+    let mut b = 2;
+    let mut sum = 0;
+    while b < under {
+        if b % 2 == 0 { sum += b; }
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    sum
+}
+
+#[test]
+fn computes_sum_of_even_fibonacci_numbers() {
+    assert_eq!(sum_of_even_fibonacci_numbers(100), 2 + 8 + 34);
+}
